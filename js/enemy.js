f1e6c9306fff1d6cc29282f7f8531e979fff6a36
Havystar <Havystar@github.com>
2018-02-01 23:07:40 +0100

    projekt angry

--- js/enemy.js
+++ js/enemy.js
@@ -1,13 +1,12 @@
-function Enemy(_type){
+function Enemy(_type) {
     this.x = Math.floor((Math.random() * canvas.width) + 10);
     this.y = Math.floor((Math.random() * canvas.height) + 10);
     this.type = _type;
     this.sighted = false;
-    //this.celWZyciu = true;
-    this.dirX =  this.x;
-    this.dirY =  this.y;
-    
-     switch(this.type){
+    this.dirX = this.x;
+    this.dirY = this.y;
+
+    switch (this.type) {
         case 0:
             this.life = 200;
             this.maxlife = 200;
@@ -19,8 +18,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false; //angry oznacza czy nas atakuje od razu czy nie
-             //Powstawiałem losowe wartości, żeby reszta kodu działała, gdy się to doczepi - B
-            
+            this.startAngry = false; //angry oznacza czy nas atakuje od razu czy nie
+            //Powstawiałem losowe wartości, żeby reszta kodu działała, gdy się to doczepi - B
+
             break;
         case 1:
             this.life = 200;
@@ -33,8 +33,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-            
+
         case 2:
             this.life = 200;
             this.maxlife = 200;
@@ -46,8 +47,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-            
+
         case 3:
             this.life = 200;
             this.maxlife = 200;
@@ -59,8 +61,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-            
+
         case 4:
             this.life = 200;
             this.maxlife = 200;
@@ -72,8 +75,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-             
+
         case 5:
             this.life = 200;
             this.maxlife = 200;
@@ -85,8 +89,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-             
+
         case 6:
             this.life = 200;
             this.maxlife = 200;
@@ -98,8 +103,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-             
+
         case 7:
             this.life = 200;
             this.maxlife = 200;
@@ -111,8 +117,9 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-             
+
         case 8:
             this.life = 200;
             this.maxlife = 200;
@@ -124,96 +131,96 @@ function Enemy(_type){
             this.rangeOffire = 0;
             this.rangeOfSee = 0;
             this.angry = false;
+            this.startAngry = false;
             break;
-                    }
+    }
 }
 Enemy.prototype.list = new Array(0);
 
-Enemy.prototype.create = function(){
+Enemy.prototype.create = function () {
     Enemy.prototype.list.push(new Enemy(_type));
 }
 
-Enemy.prototype.draw = function(){
-    if(Math.pow(this.x - this.targetX, 2) + Math.pow(this.y - this.targetY, 2) < 20 ){
-        this.Enemy.setTarget();
-}
-    //-----------------------------------------BENIO MOŻE BYĆ TAK CZY TO NIE ZADZIAŁA? tzn chodzi o samo this.Enemy zadziała
-    
-    
-    if(this.length !== 0) this.sinus = (this.targetX - this.x)/this.length;
+Enemy.prototype.draw = function () {
+
+
+    if (this.length !== 0) this.sinus = (this.targetX - this.x) / this.length;
     else this.sinus = 0;
-    this.angle = Math.asin(this.sinus) * 180/Math.PI;
+    this.angle = Math.asin(this.sinus) * 180 / Math.PI;
     //console.log("angle: "+this.angle);
     ctx.save();
     ctx.translate(this.x, this.y);
-    if(this.y < this.targetY){
-        ctx.rotate (180 * Math.PI / 180);
+    if (this.y < this.targetY) {
+        ctx.rotate(180 * Math.PI / 180);
         ctx.rotate(-this.angle * Math.PI / 180);
-    }
-    else{
+    } else {
         ctx.rotate(this.angle * Math.PI / 180);
     }
     //------------------------------------------------obracanie
 
-        switch(this.type){
-            
-            case 0:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-            case 1:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-            case 2:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-            case 3:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-            case 4:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-            case 5:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-            case 6:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-            case 7:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                
-             case 8:
-                ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
-                break;
-                        }
-    ctx.restore();
-  
-}
-Enemy.prototype.drawAll = function(){
-    for(var i = 0; i<Enemy.prototype.list.length; i++){
-        Enemy.prototype.list[i].draw();     
+    switch (this.type) {
+
+        case 0:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 1:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 2:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 3:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 4:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 5:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 6:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 7:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
+
+        case 8:
+            ctx.drawImage(this.name.image, this.x, this.y, -32 * px, -32 * px, 64 * px, 64 * px);
+            break;
     }
+    ctx.restore();
+
 }
+Enemy.prototype.drawAll = function () {
+    for (var i = 0; i < Enemy.prototype.list.length; i++) {
+        Enemy.prototype.list[i].draw();
+    }
     
-Enemy.prototype.setTarget = function(){
-    //console.log(Math.pow(this.x - e.screenX, 2) + Math.pow(this.y - e.screenY, 2));
-    
-            this.targetX = Math.floor((Math.random() * canvas.width-30) + 15);
-            this.targetY = Math.floor((Math.random() * canvas.height-30) + 15);
-    
-   
-    
-    //console.log("screenX: "+e.screenX);
-    //console.log("screenY: "+e.screenY);
 }
 
+Enemy.prototype.setTarget = function () {
+
+    this.targetX = Math.floor((Math.random() * canvas.width - 30) + 15);
+    this.targetY = Math.floor((Math.random() * canvas.height - 30) + 15);
+}
+
+function follow(){
+    
+    this.targetX = player.x - this.x;
+    this.targetY = player.y - this.y;
+    this.lenght = Math.abs(this.dx) +  Math.abs(this.dy);
+    
+}
+
+/*
 Enemy.prototype.swim = function(){
     //console.log("targetX: "+this.targetX);
     //console.log("this.x" +this.x)
@@ -236,15 +243,64 @@ Enemy.prototype.swim = function(){
         this.y += this.dirY*deltaTime*1*frame;
     }
 }
-Enemy.prototype.alive = function(){
- if(this.life<=0){
-    while(Math.abs(Ship.x-this.x)<=350 || Math.abs(Ship.y-this.y)<=350) {
-    this.x = Math.floor((Math.random() * canvas.width-30) + 15);
-    this.y = Math.floor((Math.random() * canvas.height-30) + 15);
-    }
-    money+=this.money;
-    this.life=this.maxlife;
- }
-}
-   
+*/
+Enemy.prototype.update = function () {
 
+    this.dirX += this.targetX - this.x;
+    this.dirY += this.targetY - this.y;
+    this.length = Math.sqrt(this.dirX * this.dirX + this.dirY * this.dirY); //oblicza bezpośrednią długość odcinka
+    
+    if (this.length !== 0) {
+
+        this.dirX /= this.length;
+        this.dirY /= this.length; //dzieli kierunek przez długość, żeby niezależnie od długości odcinka iśc z tą samą prędkością    
+        }
+    else {
+
+        this.dirX = 0;
+        this.dirY = 0;
+
+    }
+
+    if (this.length > 5) {
+        this.x += this.dirX * deltaTime * 1 * frame;
+        this.y += this.dirY * deltaTime * 1 * frame;
+    }
+    //podązanie i strzał jeśli go widzi
+    if (Math.sqrt(Math.pow(this.x - player.x) + Math.pow(this.y - player.y)) <= this.rangeOfSee || this.angry) {
+
+        follow();
+       // shot();
+
+    }
+
+    //sprawdza czy jest u celu jak tak to daje mu nowy cel jeśli nie widzi Graczaa i nie jest wkurzony
+    else if (Math.pow(this.x - this.targetX, 2) + Math.pow(this.y - this.targetY, 2) < 20) {
+        this.Enemy.setTarget();
+    }
+
+    //staje się zły jak oberwie
+    if (this.life < this.maxlife) {
+        this.angry = true;
+    }
+
+}
+
+Enemy.prototype.updateAll = function (DeltaTime) {
+    for (var i = 0; i < Enemy.prototype.list.length; i++) {
+        Enemy.prototype.list[i].update();
+    }
+
+}
+
+Enemy.prototype.alive = function () {
+    if (this.life <= 0) {
+        while (Math.abs(Ship.x - this.x) <= 350 || Math.abs(Ship.y - this.y) <= 350) {
+            this.x = Math.floor((Math.random() * canvas.width - 30) + 15);
+            this.y = Math.floor((Math.random() * canvas.height - 30) + 15);
+        }
+        money += this.money;
+        this.life = this.maxlife;
+        this.angry = this.startAngry;
+    }
+}
